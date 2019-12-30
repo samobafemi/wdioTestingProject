02a812e5edb819cbc4cd3309dbfca4d04aaff5a6
sam Obafemi <samobafemi@hotmail.com>
2019-12-30 22:19:05 +0000

    Added users.js

---
+++ fixtures/users.js
@@ -0,0 +1,16 @@
+const users = {
+    user1: {
+        username: "Admin",
+        password:"admin123",
+        user2: {
+            username: "batman",
+            password: "craxy1"
+        }
+    }
+
+}
+module.exports = users
+
+    
+
+
--- tests/login/orangeHRM.js
+++ tests/login/orangeHRM.js
@@ -4,11 +4,15 @@ const{expect} = require('chai')
 
 describe('OrangeHRM testing', function() {
 
-    // afterEach(function(){
-    //     LoginPage.close()
+    // beforeEach(function(){
+    //     browser.url('./')
     // })
 
     describe('Login Scenerios', function() {
+
+        beforeEach(function(){
+        browser.url('./')
+    })
 
     it('should allow you to log in using valid credentials',function(){
         LoginPage.loginToApp('Admin', 'admin123')
@@ -17,6 +21,7 @@ describe('OrangeHRM testing', function() {
 
     })
     it('should not allow you to log in using invalid credentials', function(){
+       // 
         LoginPage.openNewBrowser()
         LoginPage.loginToApp('Admin', 'admin120')
         expect(browser.getUrl()).to.contains('validateCredentials')
